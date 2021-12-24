18602fbf5db1928df49b7279b27901d46e6c9844
sunchang272 <sunch@cnu.edu.cn>
2021-12-24 15:12:04 +0800

    v1.1

--- main.py
+++ main.py
@@ -7,20 +7,47 @@ Author: sunch
 Create Date: 2021/11/18 14:49 
 -------------------------------------------------
 """
-
-import hw
+import argparse
 import webrtcns
 import time
+import hw
+
 
 ns = webrtcns.Ns()
 ns.set_mode(3)
 
-recorder = hw.recorder()
+
 player = hw.player()
 
-while True:
-    data = recorder.read(160, exception_on_overflow=False)
-    data = ns.process(data)
-    player.write(data)
-    time.sleep(.001)
+
+def record_in():
+    recorder = hw.recorder()
+    while True:
+        data = recorder.read(160, exception_on_overflow=False)
+        data = ns.process(data)
+        player.write(data)
+        time.sleep(.001)
+
+
+def file_in():
+    wf = open(args.file, 'rb')
+    wf.read(44)
+    while True:
+        data = wf.read(160)
+        data = ns.process(data)
+        player.write(data)
+        time.sleep(.001)
+
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('-m', '--mode', type=str, default='r')
+    argParser.add_argument('-f', '--file', type=str, default='./-10db_s1_car.wav')
+    args = argParser.parse_args()
+
+    if args.mode == 'r':
+        record_in()
+
+    if args.mode == 'f':
+        file_in()
 
