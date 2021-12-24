c3585afddfec886514c96199780f6e6392559c8e
sunchang272 <sunch@cnu.edu.cn>
2021-12-24 15:23:07 +0800

    v1.4

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def file_in():
         data = wf.read(160)
         data = ns.process(data)
         player.write(data)
-        time.sleep(1)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
